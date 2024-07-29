d088339b468cc0570f83a0a263b3561fa18dfac4
hilmychazone@gmail.com <hilmychazone@gmail.com>
2024-07-29 15:36:57 +0700

    maintance scan and add backup code scan

--- resources/views/scan.blade.php
+++ resources/views/scan.blade.php
@@ -12,34 +12,16 @@
 
 <!-- Styles -->
 <style>
+   
 
-#scanner-overlay {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        width: 80%; /* Adjust width as needed */
-        height: 20%;
-        border: 2px dashed red;
-        transform: translate(-50%, -50%);
-        pointer-events: none;
-        z-index: 10;
-    }
- 
-
-    canvas {
-        display: none;
-        width: 100%;
-        height: 100%;
-        display: block; /* Make canvas visible */
-    }
 </style>
 </head>
 <body>
-    <div class="mx-auto w-[428px]">
+    <div>
         <div>
             <div class="bg-green-700 opacity-700">
                 <div class="flex items-center justify-center py-4 pb-3">
-                    <h1 class=" text-center text-lg font-bold text-white tracking-wider">About us</h1>
+                    <h1 class=" text-center text-lg font-bold text-white tracking-wider">Scan</h1>
                     <div class="flex items-center absolute right-3 space-x-6">
                         <img src="/assets/icon/setting.svg" class="h-5 w-5" alt="">
                         <a href="/home">
@@ -49,24 +31,23 @@
                 </div>
             </div>
         </div>
-        <div class="fixed w-[428px] h-full">
-        <div id="reader"></div>
-            <div id="scanner-overlay"></div>
-        </div>
+    <div class="py-32">
+            <div id="reader"></div>
         <input class="max-w-screen-sm max-h-screen" type="hidden" name="result" id="result">
     </div>
+    </div>
+    <script src="https://unpkg.com/html5-qrcode" type="text/javascript"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/quagga/0.12.1/quagga.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
-        let alertShown = false; // Flag to indicate if alert has been shown
+        let alertShown = false;
 
-        function onDetected(result) {
+        function onScanSuccess(decodedText, decodedResult) {
             if (!alertShown) {
-                console.log("Barcode detected:", result.codeResult.code); // Log detected barcode
-                $('#result').val(result.codeResult.code);
-                let id = result.codeResult.code;
-                alertShown = true; // Set the flag to true
+                console.log("Barcode detected:", decodedText);
+                $('#result').val(decodedText);
+                let id = decodedText;
+                alertShown = true;
                 var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
                 $.ajax({
                     url: "{{ route('validasi') }}",
@@ -77,7 +58,7 @@
                         barcode: id
                     },
                     success: function (response) {
-                        console.log("Server response:", response); // Log server response
+                        console.log("Server response:", response);
                         if(response.status == 200){
                             Swal.fire({
                                 icon: 'success',
@@ -97,7 +78,7 @@
                         }
                     },
                     error: function(xhr, status, error) {
-                        console.log("AJAX error:", error); // Log AJAX errors
+                        console.log("AJAX error:", error);
                     }
                 });
 
@@ -107,79 +88,36 @@
             }
         }
 
-        function overrideQuaggaCanvas() {
-            const originalGetContext = HTMLCanvasElement.prototype.getContext;
-            HTMLCanvasElement.prototype.getContext = function (type, attributes) {
-                if (type === '2d' && attributes === undefined) {
-                    attributes = { willReadFrequently: true };
-                }
-                return originalGetContext.call(this, type, attributes);
-            };
-        }
-
-        overrideQuaggaCanvas();
-
-        function initializeQuagga() {
-            Quagga.init({
-                inputStream: {
-                    name: "Live",
-                    type: "LiveStream",
-                    target: document.querySelector('#reader'),
-                    constraints: {
-                        width: 430,
-                        height: 720,
-                        facingMode: "environment" 
-                    }, 
-                    area: { // defines rectangle of the detection/localization area
-    top: "0%",    // top offset
-    right: "0%",  // right offset
-    left: "0%",   // left offset
-    bottom: "0%"  // bottom offset
-  }, 
-                    singleChannel: false
-                },
-                locator: {
-                    patchSize: "medium",
-                    halfSample: true
-                },
-                numOfWorkers: 4,
-                decoder: {
-                    readers: [
-                        "code_128_reader",
-                        "upc_reader",
-                        "ean_reader",
-                        "code_39_reader"
-                    ]
-                },
-                locate: true
-            }, function(err) {
-                if (err) {
-                    console.log("Quagga initialization error:", err); 
-                    return;
-                }
-                console.log("Initialization finished. Ready to start");
-                Quagga.start();
-            });
-
-            Quagga.onProcessed(function(result) {
-                var drawingCtx = Quagga.canvas.ctx.overlay;
-                var drawingCanvas = Quagga.canvas.dom.overlay;
-                drawingCanvas.style.display = 'none'; 
-
-                $('#result').append(drawingCanvas);
-            });
-
-            Quagga.onDetected(onDetected);
-        }
-
         $(document).ready(function() {
-            initializeQuagga();
+            const html5QrCode = new Html5Qrcode("reader");
+
+            html5QrCode.start(
+                { facingMode: "environment" }, 
+                {
+                    fps: 10, 
+                    qrbox: { width: 250, height: 250 }
+                },
+                onScanSuccess)
+            .catch(err => {
+                console.log("Unable to start scanning:", err);
+            });
 
             $(window).resize(function() {
-    Quagga.stop();
-    initializeQuagga();
-});
-
+                html5QrCode.stop().then(() => {
+                    html5QrCode.start(
+                        { facingMode: "environment" },
+                        {
+                            fps: 10,
+                            qrbox: { width: 250, height: 250 }
+                        },
+                        onScanSuccess)
+                    .catch(err => {
+                        console.log("Unable to start scanning:", err);
+                    });
+                }).catch(err => {
+                    console.log("Unable to stop scanning:", err);
+                });
+            });
         });
     </script>
 </body>
